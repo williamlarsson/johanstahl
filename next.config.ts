4ca7aa1065e3c7a1135572ff4571badf89f42c2a
William Larsson <williamlarsson@live.dk>
2025-08-24 00:45:06 +0200

    reset config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  distDir: "docs",
+  distDir: "out",
   trailingSlash: true,
   images: {
     unoptimized: true,
