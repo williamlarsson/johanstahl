84580306fd30d0498cfb524b6520c258220e2acd
William Larsson <williamlarsson@live.dk>
2025-08-24 00:32:56 +0200

    rename dist to docs

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  distDir: "dist",
+  distDir: "docs",
   trailingSlash: true,
   images: {
     unoptimized: true,
